08525ee71980729e2317d6e5bd7aef84835e1f7f
midhunhk <midhunhk@gmail.com>
2014-02-20 01:21:38 +0530

    Apache HttpClient based post implementation

--- v3/PNRStatusApp/src/com/ae/apps/pnrstatus/utils/HttpUtils.java
+++ v3/PNRStatusApp/src/com/ae/apps/pnrstatus/utils/HttpUtils.java
@@ -18,16 +18,30 @@ package com.ae.apps.pnrstatus.utils;
 
 import static com.ae.apps.pnrstatus.utils.AppConstants.METHOD_POST;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 
 import android.util.Log;
 
@@ -177,5 +191,54 @@ public class HttpUtils {
 		is.close();
 		con.disconnect();
 		return buf.toString();
+	}
+
+	// http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
+	// http://www.androidsnippets.com/get-the-content-from-a-httpresponse-or-any-inputstream-as-a-string
+
+	public static String sendPost(String url, Map<String, String> requestHeaders, Map<String, String> formParameters) {
+
+		StringBuilder result = new StringBuilder();
+		try {
+			HttpPost httppost = new HttpPost(url);
+			List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+
+			// Map all the headers
+			for (String key : requestHeaders.keySet()) {
+				httppost.setHeader(key, requestHeaders.get(key));
+			}
+
+			// Adding all the params
+			for (String key : formParameters.keySet()) {
+				postParams.add(new BasicNameValuePair(key, formParameters.get(key)));
+			}
+
+			// Add your data
+			httppost.setEntity(new UrlEncodedFormEntity(postParams, HTTP.UTF_8));
+
+			// Execute HTTP Post Request
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response = httpclient.execute(httppost);
+
+			int res = response.getStatusLine().getStatusCode();
+			Log.d("PNR", "response " + res);
+			Log.d("PNR", response.getStatusLine().getReasonPhrase());
+
+			String line = "";
+
+			// Wrap a BufferedReader around the InputStream
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+			// Read response until the end
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
+		return result.toString();
 	}
 }
